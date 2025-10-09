3b94bc9a06e3770aa80c361640f846825b8d5c1a
Samuel-Rogoski <rogoski.souza@escol.pr.gov.br>
2025-10-09 07:52:34 -0300

    exercicios 2 completo

---
+++ extra.js
@@ -0,0 +1,10 @@
+let i = 1;
+while (i <= 10){
+    let j = 1;
+    while (j <= 10){
+        console.log(`${i} * ${j} = ${i*j}`);
+        j++;
+    }
+    i++;
+    console.log('--------------');
+}
